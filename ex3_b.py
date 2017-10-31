50000762566bab561b95713a7936ec291846fdd7
Nick Davis <nickdavisa@gmail.com>
2017-10-30 21:28:17 -0700

    Added comments to ex3_b.py

--- ex3_b.py
+++ ex3_b.py
@@ -2,6 +2,9 @@
 # "Find something you need to calculate and write a new .py file that does it."
 # I've chosen to calculate the first problem from Project Euler
 
+# This loops through all values from 1-1000 and gets the sum of all numbers
+# that are evenly divided by 3 or 5. A good start but there is definitely
+# more efficient ways to do this, maybe I will come back in the future.
 sum = 0
 for num in range(1000):
     if num % 3 == 0 or num % 5 == 0:
