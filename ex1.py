45b3a011924774e346d0b16ddfb7349a47d5eb11
Nick Davis <nickdavisa@gmail.com>
2017-10-30 20:39:09 -0700

    exercise 1 complete

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("Here is another line being printed.")
+# Putting a '#' in front of line creates a comment
