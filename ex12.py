e8df6e1a9c93b44af9f03cb009d286df4f8195a7
Nick Davis <nickdavisa@gmail.com>
2017-11-12 23:03:41 -0700

    exercise 12 complete

---
+++ ex12.py
@@ -0,0 +1,12 @@
+# Prompting People
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So you're {age} years old, you're {height} tall and {weight} heavy.")
+
+# Use pydoc to learn about file, os and sys.
+# File: python class that lets you manipulate files in three modes r,w,a
+# Os: provides a way to use specific operating system capabilities in python
+# Sys: provides some ability to work with the interpreter
