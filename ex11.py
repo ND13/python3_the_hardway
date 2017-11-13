c20607d06b62cae236a8aafbfd272c92597cccdd
Nick Davis <nickdavisa@gmail.com>
2017-11-12 22:51:09 -0700

    exercise 11 complete

---
+++ ex11.py
@@ -0,0 +1,18 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
+
+# Study drill 3, Write another question form using input()
+fav_movie = input("What's your favorite movie? ")
+fav_album = input("What's your favorite album? ")
+fav_book = input("What's your favorite book? ")
+
+print(f"""
+Your favorite movie is {fav_movie}, your favorite album is {fav_album} and your
+favorite book is {fav_book}.
+""")
